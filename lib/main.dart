b3f2fda51575abe7ab7f344dd68ac869a5d796e8
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-18 09:01:45 -0300

    Added commentaries

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      initialRoute:"/preload",
+      initialRoute:"/home",
       routes:{
         "/home":(context)=>HomeScreen(),
         "/list":(context)=>ListScreen(),
